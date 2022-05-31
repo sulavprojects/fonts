8feda03fe91ca2447e42eb7ba26a9018313dbd85
django <78250295+iamsulavpariyar@users.noreply.github.com>
2022-05-31 21:13:17 +0545

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn arabfonts.wsgi --log-file -
--- arabfonts/wsgi.py
+++ arabfonts/wsgi.py
@@ -11,11 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arabfonts.settings')
 
 application = get_wsgi_application()
-application = get_wsgi_application()
-application = WhiteNoise(application)
+
